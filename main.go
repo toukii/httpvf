9b73780efdccf77abbc9dd166f8693de32e115af
toukii <toukii@toukii.com>
2017-01-23 21:08:51 +0800

    auto

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"github.com/ghodss/yaml"
+	//"github.com/ghodss/yaml"
+	yaml "gopkg.in/yaml.v2"
 	"github.com/toukii/goutils"
 )
+
+type Req struct{
+	Code int
+	Cost int
+}
+
 
 func main() {
 	bs:=goutils.ReadFile("ht.yaml")
 
-	var v interface{}
+	var v Req
 	err:=yaml.Unmarshal(bs,&v)
 	goutils.CheckErr(err)
 }
