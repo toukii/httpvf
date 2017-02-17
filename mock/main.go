8ef5dcf33ecc6837f2fba8a6406b27b0486d750b
toukii <toukii@yahoo.com>
2017-02-17 16:17:11 +0800

    upload

--- mock/main.go
+++ mock/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 type Msg struct {
-	Map map[string]interface{}
+	Map     map[string]interface{}
 	Message string
 	Cost    float64
 }
@@ -19,7 +19,7 @@ func newMsg(msg string, cost float64) *Msg {
 	return &Msg{
 		Message: msg,
 		Cost:    cost,
-		Map: make(map[string]interface{}),
+		Map:     make(map[string]interface{}),
 	}
 }
 
@@ -32,23 +32,23 @@ func main() {
 	}))
 	m.Group("/", func() {
 		m.Get("", func(ctx *macaron.Context) {
-			bd,er:=ctx.Req.Body().String()
-			fmt.Println("req-body:",bd,er)
+			bd, er := ctx.Req.Body().String()
+			fmt.Println("req-body:", bd, er)
 			fmt.Println("This is toukii,root")
 			ctx.JSON(200, newMsg("This is toukii,root", 3.14))
 		})
 		m.Get("r1", func(ctx *macaron.Context) {
-			bd,er:=ctx.Req.Body().String()
-			fmt.Println("req-body:",bd,er)
+			bd, er := ctx.Req.Body().String()
+			fmt.Println("req-body:", bd, er)
 			fmt.Println("This is toukii,r1")
-			msg:=newMsg("This is toukii,r1", 0.315)
-			msg.Map["1"]="hello"
+			msg := newMsg("This is toukii,r1", 0.315)
+			msg.Map["1"] = "hello"
 			ctx.JSON(201, []*Msg{msg})
 		})
 		m.Combo("r2").Post(r2)
 		m.Post("upload", func(ctx *macaron.Context) {
-			bd,er:=ctx.Req.Body().String()
-			fmt.Println("req-body:",bd,er)
+			bd, er := ctx.Req.Body().String()
+			fmt.Println("req-body:", bd, er)
 			mul, header, err := ctx.GetFile("filename")
 			if goutils.CheckErr(err) {
 				ctx.JSON(403, err)
@@ -67,6 +67,25 @@ func main() {
 			goutils.CheckErr(err2)
 			defer mul.Close()
 
+		})
+		m.Post("upload2", func(ctx *macaron.Context) {
+			r := ctx.Req
+			r.ParseMultipartForm(32 << 20)
+			file, handler, err := r.FormFile("filename")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file.Close()
+			f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer f.Close()
+			io.Copy(f, file)
+			w := ctx.Resp
+			fmt.Fprintln(w, "upload ok!")
 		})
 	})
 	// macaron.Env = macaron.PROD
