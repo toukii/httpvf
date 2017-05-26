1b46f5d100692806fef8ed42d3f2b61cbce525da
toukii <toukii@toukii.com>
2017-05-26 17:00:01 +0800

    ReqFmt

--- req.go
+++ req.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/toukii/goutils"
 	yaml "gopkg.in/yaml.v2"
+	"io/ioutil"
+	"net/http"
 	"strings"
 )
 
@@ -37,12 +39,50 @@ type Req struct {
 	Sync     bool // 根请求（第一个请求）为异步模式，默认同一层请求为异步模式
 }
 
+func (req *Req) Do() ([]byte, error) {
+	var resp *http.Response
+	var request *http.Request
+	var err error
+	// fmt.Printf("[%s]%s\n", req.Method, req.URL)
+	if len(req.Upload) > 0 {
+		splt := strings.Split(req.Upload, "@")
+		tag := "filename"
+		filename := splt[0]
+		if len(splt) > 1 {
+			tag = splt[0]
+			filename = splt[1]
+		}
+		request, err = newfileUploadRequest(req.URL, nil, tag, filename)
+		if goutils.CheckErr(err) {
+			buf := reqBody(req.Body)
+			request, err = http.NewRequest(req.Method, req.URL, buf)
+		}
+	} else {
+		buf := reqBody(req.Body)
+		request, err = http.NewRequest(req.Method, req.URL, buf)
+	}
+	if goutils.CheckErr(err) {
+		return nil, err
+	}
+
+	for k, v := range req.Header {
+		request.Header.Add(k, v)
+	}
+
+	c := http.Client{}
+	resp, err = c.Do(request)
+	if goutils.CheckErr(err) {
+		return nil, err
+	}
+	return ioutil.ReadAll(resp.Body)
+}
+
 func Reqs(filename string) (reqs []*Req, err error) {
 	in := goutils.ReadFile(filename)
 	reqs = make([]*Req, 0, 1)
 	if len(in) > 0 && in[0] != byte('-') {
 		var req1 *Req
-		req1, err = req(in)
+		req1, err = ReqFmt(in)
 		if goutils.CheckErr(err) {
 			return nil, err
 		}
@@ -56,7 +96,7 @@ func Reqs(filename string) (reqs []*Req, err error) {
 	return reqs, nil
 }
 
-func req(in []byte) (*Req, error) {
+func ReqFmt(in []byte) (*Req, error) {
 	var req Req
 	err := yaml.Unmarshal(in, &req)
 	if goutils.CheckErr(err) {
