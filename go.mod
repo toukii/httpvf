597da7b8ca3276e27025f548f68a840eae903a89
toukii <toukii@aliyun.com>
2025-08-28 20:41:16 +0800

    mod

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/toukii/httpvf
+
+go 1.25.0
+
+require (
+	github.com/toukii/goutils v0.1.2
+	github.com/toukii/jsnm v1.0.1
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/everfore/exc v1.0.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
--- mock/main.go
+++ mock/main.go
@@ -1,73 +1,74 @@
 package main
 
-import (
-	"fmt"
-	"github.com/go-macaron/macaron"
-	"html/template"
-	"io"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"html/template"
+// 	"io"
+// 	"os"
 
-type Msg struct {
-	Map     map[string]interface{}
-	Message string
-	Cost    float64
-}
+// 	"gopkg.in/macaron.v1"
+// )
 
-func newMsg(msg string, cost float64) *Msg {
-	return &Msg{
-		Message: msg,
-		Cost:    cost,
-		Map:     make(map[string]interface{}),
-	}
-}
+// type Msg struct {
+// 	Map     map[string]interface{}
+// 	Message string
+// 	Cost    float64
+// }
 
-func main() {
-	m := macaron.Classic()
-	m.Use(macaron.Renderer(macaron.RenderOptions{
-		Funcs: []template.FuncMap{
-			{},
-		},
-	}))
-	m.Group("/", func() {
-		m.Get("", func(ctx *macaron.Context) {
-			bd, er := ctx.Req.Body().String()
-			fmt.Println(bd, er)
-			ctx.JSON(200, newMsg("This is toukii,root", 3.14))
-		})
-		m.Get("r1", func(ctx *macaron.Context) {
-			bd, er := ctx.Req.Body().String()
-			fmt.Println(bd, er)
-			msg := newMsg("This is toukii,r1", 0.315)
-			msg.Map["1"] = "hello"
-			ctx.JSON(201, []*Msg{msg})
-		})
-		m.Combo("r2").Post(r2)
-		m.Post("upload", func(ctx *macaron.Context) {
-			r := ctx.Req
-			r.ParseMultipartForm(32 << 20)
-			file, handler, err := r.FormFile("verify")
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			defer file.Close()
-			f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			defer f.Close()
-			io.Copy(f, file)
-			w := ctx.Resp
-			fmt.Fprint(w, "upload ok!")
-		})
-	})
-	 macaron.Env = macaron.PROD
-	//macaron.Env = macaron.DEV
-	m.Run()
-}
+// func newMsg(msg string, cost float64) *Msg {
+// 	return &Msg{
+// 		Message: msg,
+// 		Cost:    cost,
+// 		Map:     make(map[string]interface{}),
+// 	}
+// }
 
-func r2(ctx *macaron.Context) {
-	ctx.JSON(200, newMsg("This is toukii,r2", 0.101))
-}
+// func main() {
+// 	m := macaron.Classic()
+// 	m.Use(macaron.Renderer(macaron.RenderOptions{
+// 		Funcs: []template.FuncMap{
+// 			{},
+// 		},
+// 	}))
+// 	m.Group("/", func() {
+// 		m.Get("", func(ctx *macaron.Context) {
+// 			bd, er := ctx.Req.Body().String()
+// 			fmt.Println(bd, er)
+// 			ctx.JSON(200, newMsg("This is toukii,root", 3.14))
+// 		})
+// 		m.Get("r1", func(ctx *macaron.Context) {
+// 			bd, er := ctx.Req.Body().String()
+// 			fmt.Println(bd, er)
+// 			msg := newMsg("This is toukii,r1", 0.315)
+// 			msg.Map["1"] = "hello"
+// 			ctx.JSON(201, []*Msg{msg})
+// 		})
+// 		m.Combo("r2").Post(r2)
+// 		m.Post("upload", func(ctx *macaron.Context) {
+// 			r := ctx.Req
+// 			r.ParseMultipartForm(32 << 20)
+// 			file, handler, err := r.FormFile("verify")
+// 			if err != nil {
+// 				fmt.Println(err)
+// 				return
+// 			}
+// 			defer file.Close()
+// 			f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+// 			if err != nil {
+// 				fmt.Println(err)
+// 				return
+// 			}
+// 			defer f.Close()
+// 			io.Copy(f, file)
+// 			w := ctx.Resp
+// 			fmt.Fprint(w, "upload ok!")
+// 		})
+// 	})
+// 	macaron.Env = macaron.PROD
+// 	//macaron.Env = macaron.DEV
+// 	m.Run()
+// }
+
+// func r2(ctx *macaron.Context) {
+// 	ctx.JSON(200, newMsg("This is toukii,r2", 0.101))
+// }
